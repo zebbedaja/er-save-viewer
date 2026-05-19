49fbe5d226f05d943fc3c6db367855ae955c1bfd
Daniel Zsebedits <daniel.zsebedits@molit-service.de>
2026-05-19 21:36:57 +0200

    adds 404.html and correct base url  for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+import { copyFileSync } from 'fs'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -6,13 +7,20 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/er-save-viewer/' : '/',
   plugins: [
     vue(),
     vueDevTools(),
+    {
+      name: 'copy-404',
+      closeBundle() {
+        copyFileSync('dist/index.html', 'dist/404.html')
+      },
+    },
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
