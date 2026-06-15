78e54f5e9cba3a12734a12353834fbfcd1610378
Daniel Zsebedits <daniel.zsebedits@molit-service.de>
2026-06-16 01:56:23 +0200

    moves images

--- src/components/CharacterImage.vue
+++ src/components/CharacterImage.vue
@@ -1,35 +1,35 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 
-import erVagabondMale from '@/assets/img/er-vagabond-male.png'
-import erVagabondFemale from '@/assets/img/er-vagabond-female.png'
+import erVagabondMale from '@/assets/img/classes/er-vagabond-male.png'
+import erVagabondFemale from '@/assets/img/classes/er-vagabond-female.png'
 
-import erWarriorMale from '@/assets/img/er-warrior-male.png'
-import erWarriorFemale from '@/assets/img/er-warrior-female.png'
+import erWarriorMale from '@/assets/img/classes/er-warrior-male.png'
+import erWarriorFemale from '@/assets/img/classes/er-warrior-female.png'
 
-import erHeroMale from '@/assets/img/er-hero-male.png'
-import erHeroFemale from '@/assets/img/er-hero-female.png'
+import erHeroMale from '@/assets/img/classes/er-hero-male.png'
+import erHeroFemale from '@/assets/img/classes/er-hero-female.png'
 
-import erBanditMale from '@/assets/img/er-bandit-male.png'
-import erBanditFemale from '@/assets/img/er-bandit-female.png'
+import erBanditMale from '@/assets/img/classes/er-bandit-male.png'
+import erBanditFemale from '@/assets/img/classes/er-bandit-female.png'
 
-import erAstrologerMale from '@/assets/img/er-astrologer-male.png'
-import erAstrologerFemale from '@/assets/img/er-astrologer-female.png'
+import erAstrologerMale from '@/assets/img/classes/er-astrologer-male.png'
+import erAstrologerFemale from '@/assets/img/classes/er-astrologer-female.png'
 
-import erProhpetMale from '@/assets/img/er-prohpet-male.png'
-import erProhpetFemale from '@/assets/img/er-prohpet-female.png'
+import erProhpetMale from '@/assets/img/classes/er-prohpet-male.png'
+import erProhpetFemale from '@/assets/img/classes/er-prohpet-female.png'
 
-import erSamuraiMale from '@/assets/img/er-samurai-male.png'
-import erSamuraiFemale from '@/assets/img/er-samurai-female.png'
+import erSamuraiMale from '@/assets/img/classes/er-samurai-male.png'
+import erSamuraiFemale from '@/assets/img/classes/er-samurai-female.png'
 
-import erPrisonerMale from '@/assets/img/er-prisoner-male.png'
-import erPrisonerFemale from '@/assets/img/er-prisoner-female.png'
+import erPrisonerMale from '@/assets/img/classes/er-prisoner-male.png'
+import erPrisonerFemale from '@/assets/img/classes/er-prisoner-female.png'
 
-import erConfessorMale from '@/assets/img/er-confessor-male.png'
-import erConfessorFemale from '@/assets/img/er-confessor-female.png'
+import erConfessorMale from '@/assets/img/classes/er-confessor-male.png'
+import erConfessorFemale from '@/assets/img/classes/er-confessor-female.png'
 
-import erWretchMale from '@/assets/img/er-wretch-male.png'
-import erWretchFemale from '@/assets/img/er-wretch-female.png'
+import erWretchMale from '@/assets/img/classes/er-wretch-male.png'
+import erWretchFemale from '@/assets/img/classes/er-wretch-female.png'
 
 interface CharInfo {
   archetype?: number
