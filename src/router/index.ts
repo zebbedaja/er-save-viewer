6c33dd7772f995b80ffcb20a8641c8af5692ea95
Daniel Zsebedits <daniel.zsebedits@molit-service.de>
2026-09-05 16:01:50 +0200

    minor fixes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import BossList from '../components/BossList.vue'
 import BossDetail from '../components/BossDetail.vue'
 import EventCategoryDetail from '../components/EventCategoryDetail.vue'
