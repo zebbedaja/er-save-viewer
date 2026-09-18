e9cb61a1dffc0a7b3a300c52e5dcc3aba4adfa9a
Daniel Zsebedits <daniel.zsebedits@molit-service.de>
2026-09-18 19:46:48 +0200

    adds maliketh youtube video

--- src/model/youtube.ts
+++ src/model/youtube.ts
@@ -20,6 +20,12 @@ export const YOUTUBE_VIDEOS: YouTubeVideo[] = [
     youTubeUser: 'Zebbedaja',
   },
   {
+    flagId: 13000800,
+    youTubeId: 'Fj-luGTUn4g',
+    youTubeTitle: 'Maliketh RL1+0 NO HIT',
+    youTubeUser: 'Zebbedaja',
+  },
+  {
     flagId: 15000800,
     youTubeId: 'Ua3J8bmekM0',
     youTubeTitle: 'PERFECT Malenia - Level 1 Wretch (RL1, No Hit)',
